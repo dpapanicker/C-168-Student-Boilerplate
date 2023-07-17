53efc8e1c7c1e12ec4ae6b7520063679984eae14
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2023-07-17 17:57:14 +0530

    Add files via upload

---
+++ js/addButtons.js
@@ -0,0 +1,17 @@
+AFRAME.registerComponent("create-buttons", {
+    init: function() {
+      // 1. Create the button
+
+
+      
+      // 2. Create the button
+
+
+      
+      // 2. Append button elements
+
+      
+      
+    }
+  });
+  
